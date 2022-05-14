c8d86d21e0d7497c6a7b16277178b99d5cd744b9
Peteresis <98360572+Peteresis@users.noreply.github.com>
2022-05-14 19:05:42 -0400

    2022-05-14 19:05

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoicGV0ZXJlc2lzIiwiYSI6ImNsMnFyYWExMjAwZXAzYnBlZTMzZnhtaWMifQ.hL_JOhgRGQkG-5ga7DTK4w";
+const API_KEY = "pk.eyJ1IjoicGV0ZXJlc2lzIiwiYSI6ImNsMzZmYjdsYjAwNmEzYm12Y2FyaDZhNG4ifQ.__W1eXRayAis_zqYgAGb_w";
